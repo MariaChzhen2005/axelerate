4bad17fc208d3e601362c1de2635ff551fc7cc2b
programmer-oh-yeah <46852765+programmer-oh-yeah@users.noreply.github.com>
2020-05-17 08:15:07 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,15 +1,62 @@
 <!DOCTYPE html>
 <html>
-    <title>Axelerate - Games catalogue</title>
-    <meta name="description" content="Find games to play with Axelerate.com! Play fun games here.">
+    <title>Axelerate - Random activities</title>
+    <meta name="description" content="Find activities to do with Axelerate.com! ">
     <?php include("nav.html"); ?>
+    <?php include("activities-generator.html"); ?>
     <h3>Recommended for you</h3>
     <div class="grid3">
         <fieldset>
-        <a href="https://axelerate.space/games/flappy-bird">
+            <a href="https://readermaria.com/movies/spirited-away-2001">
+            <h3>Spirited Away - a movie to rewatch</h3>
+            <img src="spirited-away-2001.jpg">
+            <p>Click to read the film review</p>
+        </a>
+        </fieldset>
+        <fieldset>
+            <a href="https://axelerate.space/games/flappy-bird">
             <h3>Flappy Bird - Relax on quarantine</h3>
             <img src="flappy-bird.jpg">
             <p>Click to play the Flappy Bird game</p>
         </a>
         </fieldset>
+        <fieldset>
+            <a href="https://mlh.io/">
+            <h3>Attend a weekly Hackathon on MLH!</h3>
+            <img src="hackathon.jpg">
+            <p>Click to become an awesome coder ;)</p>
+        </a>
+        </fieldset>
+        <fieldset>
+            <a href="https://readermaria.com/freelancing">
+            <h3>Start freelancing - now is the time!</h3>
+            <img src="freelance.jpg">
+            <p>Click to read all facts about freelance</p>
+        </a>
+        </fieldset>
+
+        <fieldset>
+            <a href="https://www.youtube.com/learning">
+            <h3>Become a scientist with YouTube Learning</h3>
+            <img src="yt-learning.png">
+            <p>Click to go on YouTube Learning</p>
+        </a>
+        </fieldset>
+        <fieldset>
+            <a href="https://readermaria.com/procreate">
+            <h3>Learn to draw digitally on Procreate</h3>
+            <img src="procreate.jpg">
+            <p>Click for a tutorial on Procreate</p>
+        </a>
+        </fieldset>
+        <fieldset>
+            <a href="https://readermaria.com/books/behave-robert-sapolsky">
+            <h3>Read Behave by Robert Sapolsky</h3>
+            <img src="behave-robert-sapolsky.jpg">
+            <p>Click to read the book review!</p>
+        </a>
+        </fieldset>
+
+    </div>
     <?php include("footer.html"); ?>
+
